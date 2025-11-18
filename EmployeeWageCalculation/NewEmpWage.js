80c1339a9a4a537be620db39d62480897510ee3a
dkalki-19 <dkalkicdhandapani@gmail.com>
2025-11-18 16:52:14 +0530

    Calculate Wages till a condition of total working hours of 160 or max days of 20 is reached for a month

--- EmployeeWageCalculation/NewEmpWage.js
+++ EmployeeWageCalculation/NewEmpWage.js
@@ -65,3 +65,18 @@ let empWage = empHrs*WAGE_PER_HOUR;
 console.log("UC4 - Total Hrs : "+empHrs+" Emp Wage : "+empWage);
 
 }
+
+// UC - 5
+{
+    const MAX_HRS_IN_MONTH = 100;
+    const NO_OF_WORKING_DAYS = 10;
+    let totalEmpHrs = 0;
+    let totalWOrkingDays = 0;
+    while(totalEmpHrs<=MAX_HRS_IN_MONTH && totalWOrkingDays < NUM_OF_WORKING_DAYS){
+        totalWOrkingDays++;
+        let empCheck = Math.floor(Math.random()*10)%3;
+        totalEmpHrs+=getComputedStyle(empCheck);
+    }
+    let empWage = totalEmpHrs*WAGE_PER_HOUR;
+    console.log("UC5 - Total Days : "+totalWOrkingDays+" Emp Wage : "+empWage);
+}
