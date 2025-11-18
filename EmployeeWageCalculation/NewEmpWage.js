a1c477eed6abe219c0204773b993c60f24845b8c
dkalki-19 <dkalkicdhandapani@gmail.com>
2025-11-18 16:47:03 +0530

    Calculating Wages for a Month assuming 20 Working Days in a Month

--- EmployeeWageCalculation/NewEmpWage.js
+++ EmployeeWageCalculation/NewEmpWage.js
@@ -52,3 +52,16 @@ empHrs = getWorkingHours(empCheck);
 let empWage = empHrs * WAGE_PER_HOUR;
 console.log("Emp Wage : "+empWage);
 }
+
+// UC - 4
+{
+const NUM_OF_WORKING_DAYS = 2;
+let empHrs = 0;
+for(let day=0;day<NUM_OF_WORKING_DAYS;day++){
+    let empCheck = Math.floor(Math.random()*10)%3;
+    empHrs+=getComputedStyle(empCheck);
+}
+let empWage = empHrs*WAGE_PER_HOUR;
+console.log("UC4 - Total Hrs : "+empHrs+" Emp Wage : "+empWage);
+
+}
